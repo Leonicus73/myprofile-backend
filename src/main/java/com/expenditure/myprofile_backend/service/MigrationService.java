b837fc4b85799b7605ac05953dedbb8c1b627826
Stephen <divright@singnet.com.sg>
2025-10-24 14:39:46 +0800

    Initial commit: backend with external pswd

--- src/main/java/com/expenditure/myprofile_backend/service/MigrationService.java
+++ src/main/java/com/expenditure/myprofile_backend/service/MigrationService.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import java.util.Arrays;
 import java.util.List;
-import java.util.UUID;
+// import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Service
